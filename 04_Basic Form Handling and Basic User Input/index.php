2a96809aced189164d1750698fc7250f4a9d4e4f
SameerFaisal <sameerfaisal46@gmail.com>
2023-07-18 11:12:21 +0500

    2023-07-18

---
+++ 04_Basic Form Handling and Basic User Input/index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  </head>
+  <body>
+    
+<!-- <form action="./welcome.php" method="post">
+Email: <input type="email" name="email" placeholder="Enter your email here" required>
+<br><br>
+Password: <input type="password" name="password" placeholder="Enter Password" required>
+
+<br><br>
+<input class="btn btn-dark" type="submit" value="Submit">
+</form> -->
+
+
+<form action="welcome.php" method="post">
+
+<!-- Number 1: <input type="number" name="n1" id="">
+Number 2: <input type="number" name="n2" id="">
+<br>
+<input class="btn btn-dark" type="submit" value="Add"> -->
+
+
+Age: <input type="number" name="age" id="">
+<input class="btn btn-dark" type="submit" value="Convert">
+
+</form>
+
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ 04_Basic Form Handling and Basic User Input/welcome.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  </head>
+  <body>
+  <!-- <h1>
+
+  <?php
+   
+   $check= $_POST["email"];
+   
+   echo $check
+   
+  ?>
+  </h1> 
+
+  <h1>
+
+  <?php
+   
+   $ps= $_POST["password"];
+   
+   echo $ps
+   
+  ?>
+  </h1>  -->
+
+  <h1>
+    <?php
+    // $num1=$_POST["n1"];
+    // $num2=$_POST["n2"];
+
+    // $result=intval($num1)+intval($num2);
+    // echo $result;
+    // $age=$_POST["age"];
+    // echo $age*365
+
+    // $age=$_POST["age"];
+    // echo $age*(365*24*3600)
+
+    $x=11;
+
+    echo "I am in grade $x"
+    ?>
+  </h1>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </body>
+</html>
