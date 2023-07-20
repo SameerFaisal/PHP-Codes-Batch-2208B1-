102b0a0cf8c14e24024370ff105817a924ce8662
SameerFaisal <sameerfaisal46@gmail.com>
2023-07-20 10:38:46 +0500

    2023-07-20

---
+++ 05_Loops in PHP/index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  </head>
+  <body>
+   <h1>Hello World</h1>
+        <?php
+        $name='Sameer';
+    //    for($i=0;$i<=5;$i++){
+    //     echo "<p>$i</p>";
+    //    }
+
+    //      for($i=0;$i>5;$i++){
+    //     echo "<p>$i</p>";
+    //    }
+
+    // $i=0;
+    // while($i<5){
+    //     echo "<p>$i</p>";
+    //     $i++;
+
+    // }
+// $i=6;
+//     do{
+//         echo "<p>$i</p>";
+//         $i++;
+//     }while($i>5)
+
+// for($i=1;$i<11;$i++){
+//     $result=$i*5;
+// print("5 x $i = $result");
+// echo "<br>";
+// }
+
+// $names=array("Zain",'Hassan',"Taj ul Islam");
+// echo $names[0];
+// echo "<br>";
+// echo $names[1];
+// echo "<br>";
+// echo $names[2];
+// echo "<br>";
+
+// for($i=0;$i<3;$i++){
+//     echo "Name: ".$names[$i]."<br>";
+// }
+
+// echo var_dump($names);
+// $data=array("name"=>"Hassan","age"=>12,"contact"=>"12345");
+
+// echo var_dump($data);
+
+// $data=array("Hassan",20,array("sleeping","Awaragardi",array("Cricket","Football")));
+// echo var_dump($data);    
+// echo $data[2][2][1]        ?>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </body>
+</html>
